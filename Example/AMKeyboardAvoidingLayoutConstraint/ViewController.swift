be61d30e999d68e83e7dce1b70d9c92095d0285c
Anthony Miller <anthony@app-order.com>
2017-06-01 17:08:33 -0700

    Refactored to calculate correct constant changes based on view coordinates. Fixed bugs on rotation and when application moves to background.

--- Example/AMKeyboardAvoidingLayoutConstraint/ViewController.swift
+++ Example/AMKeyboardAvoidingLayoutConstraint/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
   
-  @IBAction func didTapView(sender: AnyObject) {
+  @IBAction func didTapView(_ sender: AnyObject) {
     view.endEditing(true)
   }
 
